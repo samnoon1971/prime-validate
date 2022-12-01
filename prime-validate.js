3eef272e298c17cb5ab7b6f48f1b8d63bda5b3e7
samnoon.abrar <samnoonabrar@gmail.com>
2022-12-01 21:08:27 +0600

    changed primality algo to trial division

--- prime-validate.js
+++ prime-validate.js
@@ -1,45 +1,9 @@
 
-
-
-
-
-
-
-
-
-function FermatFactors(n)
-{
-
-    // since fermat's factorization applicable
-    // for odd positive integers only
-    if(n <= 0 || (n % 2) === 0)
-    {
-        return false;
-    }
-
-
-    let a = Math.ceil(Math.sqrt(n)) ;
-
-    // if n is a perfect root,
-    // then both its square roots are its factors
-    if(a * a === n)
-    {
-        return false;
-    }
-    let b = 0;
-    while(true)
-    {
-        let b1 = a * a - n ;
-        b = parseInt(Math.sqrt(b1), 10);
-
-        if(b * b === b1)
-            break;
-        else
-            a += 1;
-    }
-    let firstOdd = a - b, secondOdd = a + b;
-    return (firstOdd === 1 || secondOdd === 1);
-
+function prime_validate(n) {
+   for(let d=2; d * d <= n; d++) {
+       if(n % d === 0) return false;
+   }
+   return true;
 }
 
-module.exports = FermatFactors;
+module.exports = prime_validate
